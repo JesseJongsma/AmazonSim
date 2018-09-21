186e030945bf99d4e7d37386202ffb92957796bc
Robin Hogerhuis <hoge1701@student.nhl.nl>
2018-09-21 13:15:44 +0200

    Added file Robots

--- AmazonSimulator VS/Models/Robots.cs
+++ AmazonSimulator VS/Models/Robots.cs
@@ -8,47 +8,19 @@ namespace Models
     public class Robots : Model3D, IUpdatable
     {
         private int c = 0;
-        private List<Nodes> Visited = new List<Nodes>();
-        private List<Nodes> UnVisited = new List<Nodes>();
-        private double Speed = 1;
-        public Nodes Nodes;
-
-        public Robots(Nodes nodes, string type, double x, double y, double z, double rotationX, double rotationY, double rotationZ) : base(type, x, y, z, rotationX, rotationY, rotationZ)
+        public Robots(string type, double x, double y, double z, double rotationX, double rotationY, double rotationZ) : base(type, x, y, z, rotationX, rotationY, rotationZ)
         {
             Console.WriteLine("Robot created");
-            Nodes = nodes;
-            UnVisited = Nodes.GetNodes;
-            //Move(UnVisited.First().GetX, 0.05, UnVisited.First().GetZ);
-            //transferNode(UnVisited.First());
         }
 
-        public void moveRobot()
+        public void moveRobot(Model3D model)
         {
-            Move(x, 0.05, z);
+            model.Move(model.x, model.y, model.z);
         }
 
-        public void GetShortestPath()
+        public void DijkstraAlgorithm()
         {
 
-        }
-
-        private void GetPaths()
-        {
-
-        }
-
-        private void transferNode(Nodes node)
-        {
-            if (Visited.Contains(node))
-            {
-                UnVisited.Add(node);
-                Visited.Remove(node);
-            }
-            else if (UnVisited.Contains(node))
-            {
-                Visited.Add(node);
-                UnVisited.Remove(node);
-            }
         }
     }
 }
