d125783b143dec7e0853072691e0295c8a10c39c
Jesse Jongsma <jessejongsma@hotmail.com>
2018-09-26 15:54:07 +0200

    Fixed some spaghetti code, boi

--- AmazonSimulator VS/Models/Racks.cs
+++ AmazonSimulator VS/Models/Racks.cs
@@ -7,11 +7,9 @@ namespace Models
 {
     public class Racks : Model3D, IUpdatable
     {
-        private double y; 
         public Racks(string type, double x, double y, double z, double rotationX, double rotationY, double rotationZ) : base(type, x, y, z, rotationX, rotationY, rotationZ)
         {
             Console.WriteLine("Rack created");
-            this.y = y; 
         }
 
         public void moveRack()
